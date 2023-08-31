f464c77ee995a207aebb62558b7c8b8881a87756
carmor <info@acreativo.com>
2023-08-31 22:59:01 +0200

    V9

--- bot.py
+++ bot.py
@@ -3,34 +3,21 @@ import stripe
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 
-# Configura tu clave secreta de API de Stripe
-stripe.api_key = "sk_live_sWrF2uPsg8pBfIiPPPNxPHH4"  # Reemplaza con tu clave secreta de Stripe
+# Configurar tu clave secreta de API de Stripe
+stripe.api_key = "sk_live_sWrF2uPsg8pBfIiPPPNxPHH4"
 
 def start(update: Update, context: CallbackContext) -> None:
-    message = (
-        "Suscripción mensual Premium\n\n"
-        "Acceso mensual a nuestro grupo privado de Telegram\n\n"
-        "TUS BENEFICIOS:\n"
-        "✅ NEUROEMPRENDEDOR (Acceso al grupo + 2 grupos adicionales)\n\n"
-        "Price: 8,50 €\n"
-        "Periodo: 1 month\n"
-        "Modo: Recurrente\n\n"
-        "Seleccione una opción:"
-    )
-
     keyboard = [
-        [InlineKeyboardButton("Tarjeta de Crédito", callback_data='stripe')]
+        [InlineKeyboardButton("Pagar", callback_data='stripe')]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(message, reply_markup=reply_markup)
+    update.message.reply_text("Haz clic en el botón para realizar el pago:", reply_markup=reply_markup)
 
 def button_click(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
-    query.answer()  # Acknowledge the button click
+    query.answer()
 
     if query.data == 'stripe':
-        # Crear un enlace de Checkout de Stripe
         session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[
@@ -40,27 +27,21 @@ def button_click(update: Update, context: CallbackContext) -> None:
                 }
             ],
             mode='payment',
-            success_url='https://tu-sitio.com/success',
-            cancel_url='https://tu-sitio.com/cancel',
+            success_url='https://www.google.com',
+            cancel_url='https://www.carlosmorenoo.com',
         )
         
-        # Enviar un mensaje al usuario con el enlace de pago
         payment_url = session.url
         query.message.reply_text(f"Haz clic en el enlace para realizar el pago: {payment_url}")
 
 def main():
-    # Token de acceso de tu bot
-    token = "6307738962:AAEOr7Xel_u9t_vL1SFDcK-7iTFv26lYHzY"  # Reemplaza con tu token real
+    token = "6307738962:AAEOr7Xel_u9t_vL1SFDcK-7iTFv26lYHzY"
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
     
-    # Manejador para el comando /start
     dispatcher.add_handler(CommandHandler("start", start))
-    
-    # Manejador para los botones
     dispatcher.add_handler(CallbackQueryHandler(button_click))
     
-    # Iniciar el bot
     updater.start_polling()
     updater.idle()
 
