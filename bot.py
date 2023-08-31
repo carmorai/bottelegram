4fc9c02dd33e5f70b49a235c8805efacd90f7533
carmor <info@acreativo.com>
2023-08-31 13:05:36 +0200

    V9

--- bot.py
+++ bot.py
@@ -1,17 +1,14 @@
 import os
 import stripe
 import secrets
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, Filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 
 # Configurar tu clave secreta de API de Stripe
 stripe.api_key = "sk_live_sWrF2uPsg8pBfIiPPPNxPHH4"  # Reemplaza con tu clave secreta de Stripe
 
 # Generar una clave secreta para la generación de enlaces temporales
 TEMP_LINK_SECRET = "X00CLAVESECRETAUNI68XXxx:"  # Cambia esto a tu propia clave secreta
-
-# Diccionario para almacenar la información del usuario
-user_info = {}
 
 def generate_temp_link(user_id):
     return secrets.token_urlsafe(32)  # Genera un token seguro para el enlace temporal
@@ -29,67 +26,51 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
     keyboard = [
-        [InlineKeyboardButton("Tarjeta de Crédito", callback_data='collect_info')]
+        [InlineKeyboardButton("Tarjeta de Crédito", callback_data='stripe')]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(message, reply_markup=reply_markup)
 
-def collect_info(update: Update, context: CallbackContext) -> None:
+def button_click(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     query.answer()  # Acknowledge the button click
-    
-    user_info[query.message.chat.id] = {"chat_id": query.message.chat.id}
-    
-    message = "Por favor, ingresa los siguientes datos:\n\n"
-    message += "Número de tarjeta (sin espacios ni guiones):\n"
-    query.message.reply_text(message)
-    
-    context.user_data["collect_step"] = "card_number"
 
-def process_info(update: Update, context: CallbackContext) -> None:
-    chat_id = update.message.chat.id
-    step = context.user_data.get("collect_step")
-    
-    if step == "card_number":
-        user_info[chat_id]["card_number"] = update.message.text
-        context.user_data["collect_step"] = "name"
-        update.message.reply_text("Ingresa tu nombre y apellidos:")
-    elif step == "name":
-        user_info[chat_id]["name"] = update.message.text
-        context.user_data["collect_step"] = "phone"
-        update.message.reply_text("Ingresa tu número de teléfono:")
-    elif step == "phone":
-        user_info[chat_id]["phone"] = update.message.text
-        context.user_data["collect_step"] = "message"
-        update.message.reply_text("Ingresa un mensaje adicional (opcional):")
-    elif step == "message":
-        user_info[chat_id]["message"] = update.message.text
-        context.user_data.pop("collect_step")
-        create_subscription(chat_id)
+    if query.data == 'stripe':
+        # Crear una suscripción en Stripe
+        customer = stripe.Customer.create(email=query.message.chat.username)
+        subscription = stripe.Subscription.create(
+            customer=customer.id,
+            items=[
+                {"price": "price_1Nl7wQFp9Pnzoti4T2M1C1Ly"}  # Reemplaza con el ID del precio que creaste en Stripe
+            ]
+        )
+        
+        # Generar un enlace temporal de invitación único
+        temp_link = generate_temp_link(query.message.chat.id)
+        invite_link = f'https://t.me/+QO3qSol1tMwwN2E8?start={temp_link}'
+        
+        # Enviar un mensaje al usuario con el enlace de pago y el enlace temporal
+        payment_url = subscription.latest_invoice.hosted_invoice_url
+        message = f"Haz clic en el enlace para realizar el pago: {payment_url}\n\n"
+        message += f"O utiliza este enlace temporal para unirte al grupo: {invite_link}"
+        query.message.reply_text(message)
 
-def create_subscription(chat_id):
-    # Crear una suscripción en Stripe
-    customer = stripe.Customer.create(email=user_info[chat_id]["chat_id"])
-    subscription = stripe.Subscription.create(
-        customer=customer.id,
-        items=[
-            {"price": "price_1Nl7wQFp9Pnzoti4T2M1C1Ly"}  # Reemplaza con el ID del precio que creaste en Stripe
-        ]
-    )
+def main():
+    # Token de acceso de tu bot
+    token = "6307738962:AAEOr7Xel_u9t_vL1SFDcK-7iTFv26lYHzY"  # Reemplaza con tu token real
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    # Manejador para el comando /start
+    dispatcher.add_handler(CommandHandler("start", start))
+    
+    # Manejador para los botones
+    dispatcher.add_handler(CallbackQueryHandler(button_click))
+    
+    # Iniciar el bot
+    updater.start_polling()
+    updater.idle()
 
-    # Generar un enlace temporal de invitación único
-    temp_link = generate_temp_link(chat_id)
-    invite_link = f'https://t.me/+QO3qSol1tMwwN2E8?start={temp_link}'
-    
-    # Enviar un mensaje al usuario con el enlace de pago y el enlace temporal
-    payment_url = subscription.latest_invoice.hosted_invoice_url
-    message = f"Haz clic en el enlace para realizar el pago: {payment_url}\n\n"
-    message += f"O utiliza este enlace temporal para unirte al grupo: {invite_link}"
-    message += "\n\nGracias por proporcionar la información. Tu suscripción está en proceso."
-    
-    # Limpiar la información del usuario
-    user_info.pop(chat_id)
-    
-    # Enviar el mensaje
-    context.bot
+if __name__ == "__main__":
+    main()
