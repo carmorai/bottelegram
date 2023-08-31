13f85c0b273fa701659007ca375e670f22ffe150
carmor <info@acreativo.com>
2023-08-31 23:11:55 +0200

    V9

--- bot.py
+++ bot.py
@@ -1,55 +1,67 @@
+import os
 import stripe
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 
-# Configura tu clave secreta de API de Stripe
-stripe.api_key = "sk_live_sWrF2uPsg8pBfIiPPPNxPHH4"
+# Configurar tu clave secreta de API de Stripe
+stripe.api_key = "sk_live_sWrF2uPsg8pBfIiPPPNxPHH4"  # Reemplaza con tu clave secreta de Stripe
 
 def start(update: Update, context: CallbackContext) -> None:
-    message = "Haz clic en el botón para realizar el pago."
+    message = (
+        "Suscripción mensual Premium\n\n"
+        "Acceso mensual a nuestro grupo privado de Telegram\n\n"
+        "TUS BENEFICIOS:\n"
+        "✅ NEUROEMPRENDEDOR (Acceso al grupo + 2 grupos adicionales)\n\n"
+        "Price: 8,50 €\n"
+        "Periodo: 1 month\n"
+        "Modo: Recurrente\n\n"
+        "Seleccione una opción:"
+    )
+
     keyboard = [
-        [InlineKeyboardButton("Pagar", callback_data='stripe')]
+        [InlineKeyboardButton("Tarjeta de Crédito", callback_data='stripe')]
     ]
+
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(message, reply_markup=reply_markup)
 
 def button_click(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
-    query.answer()
+    query.answer()  # Acknowledge the button click
 
     if query.data == 'stripe':
-        print("Botón de pago presionado")
-        try:
-            session = stripe.checkout.Session.create(
-                payment_method_types=['card'],
-                line_items=[
-                    {
-                        'price': 'price_1Nl7wQFp9Pnzoti4T2M1C1Ly',
-                        'quantity': 1,
-                    }
-                ],
-                mode='payment',
-                success_url='https://tu-sitio.com/pago_exitoso',
-                cancel_url='https://tu-sitio.com/pago_cancelado',
-            )
-            
-            payment_url = session.url
-            query.message.reply_text(f"Haz clic en el enlace para realizar el pago: {payment_url}")
-        except stripe.error.StripeError as e:
-            print("Error de Stripe:", str(e))
-            query.message.reply_text("Ocurrió un error de pago con Stripe. Por favor, inténtalo más tarde.")
-        except Exception as e:
-            print("Error inesperado:", str(e))
-            query.message.reply_text("Ocurrió un error inesperado. Por favor, inténtalo más tarde.")
+        # Crear un enlace de Checkout de Stripe
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price': 'price_1Nl7wQFp9Pnzoti4T2M1C1Ly',
+                    'quantity': 1,
+                }
+            ],
+            mode='payment',
+            success_url='https://tu-sitio.com/success',
+            cancel_url='https://tu-sitio.com/cancel',
+        )
+        
+        # Enviar un mensaje al usuario con el enlace de pago
+        payment_url = session.url
+        message = f"Haz clic en el enlace para realizar el pago: {payment_url}"
+        query.message.reply_text(message)
 
 def main():
-    token = "6307738962:AAEOr7Xel_u9t_vL1SFDcK-7iTFv26lYHzY"
+    # Token de acceso de tu bot
+    token = "6307738962:AAEOr7Xel_u9t_vL1SFDcK-7iTFv26lYHzY"  # Reemplaza con tu token real
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
     
+    # Manejador para el comando /start
     dispatcher.add_handler(CommandHandler("start", start))
+    
+    # Manejador para los botones
     dispatcher.add_handler(CallbackQueryHandler(button_click))
     
+    # Iniciar el bot
     updater.start_polling()
     updater.idle()
 
