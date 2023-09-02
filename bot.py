d3ba4755bdf63abc39d2e31fd98111d6e0d4cbde
carmor <info@acreativo.com>
2023-09-02 11:02:02 +0200

    V9

--- bot.py
+++ bot.py
@@ -10,15 +10,18 @@ def start(update: Update, context: CallbackContext) -> None:
         "Suscripción mensual Premium\n\n"
         "Acceso mensual a nuestro grupo privado de Telegram\n\n"
         "TUS BENEFICIOS:\n"
-        "✅ NEUROEMPRENDEDOR (Acceso al grupo + 2 grupos adicionales)\n\n"
+        "✅ WEBINARS MENSUALES EXCLUSIVOS sobre (AI, Neuroventas, Neuromarketing, Web...)\n\n"
+        "✅ CONSULTORÍA EN DIRECTO todo el mes (Os ayudo todo el mes por chat)\n\n"
+        "✅ COMUNIDAD ACTIVA (para aprender todos de todos)\n\n"
+        "✅ SORTEOS MENSUALES DE MIS LIBROS Y EVENTOS\n\n"
         "Precio: 8,50€\n"
         "Periodo: 1 mes\n"
         "Modo: Recurrente\n\n"
-        "Seleccione una opción:"
+        "Selecciona una opción:"
     )
 
     keyboard = [
-        [InlineKeyboardButton("Tarjeta de Crédito", url='https://buy.stripe.com/test_fZe7sA3nCbfW5ri288')]
+        [InlineKeyboardButton("Tarjeta de Crédito o PAYPAL", url='https://buy.stripe.com/test_fZe7sA3nCbfW5ri288')]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
